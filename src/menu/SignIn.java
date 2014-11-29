0b57c7e812217660e4c8aa3ccac38c3726cc9def
Cho YuJin <gina1768@naver.com>
2014-11-29 18:11:18 +0900

    므ㅏ아아아

--- src/menu/SignIn.java
+++ src/menu/SignIn.java
@@ -6,5 +6,6 @@ public class SignIn {
 		System.out.println("°¡ÀÔÇÏ±â");
 		System.out.println("Á¶À¯ Ãß°¡");
 		System.out.println("È÷È÷È÷ÆR ¾î·Á¿ö Á×°Ú³×");
+		System.out.println("¿ìÇìÇìÇìÇì");
 	}
 }
