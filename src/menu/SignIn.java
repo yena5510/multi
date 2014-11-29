c86de9f912c28d59bd9c11fe33e7a9e6ade78be1
Cho YuJin <gina1768@naver.com>
2014-11-29 18:06:49 +0900

    어려워 죽겄오

--- src/menu/SignIn.java
+++ src/menu/SignIn.java
@@ -4,7 +4,7 @@ public class SignIn {
 
 	public static void main(String[] args){
 		System.out.println("°¡ÀÔÇÏ±â");
-		
 		System.out.println("Á¶À¯ Ãß°¡");
+		System.out.println("È÷È÷È÷ÆR ¾î·Á¿ö Á×°Ú³×");
 	}
 }
