19a4a26877a63481c7ddb2885ea06f21181ad064
Cho YuJin <gina1768@naver.com>
2014-12-04 20:40:32 +0900

    Success to login and exception process when Id and Password are not match

--- MainMenu.java
+++ MainMenu.java
@@ -7,18 +7,22 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MainMenu {
-	
+	 String st_id;
+     String st_pw;
+     String st_name;
+     String st_major;
+     String st_type;    
 	public static Connection makeConnection(){
-        String url = "jdbc:mysql://localhost:3307/booktest";
+        String url = "jdbc:mysql://localhost:3307/library";
         String id = "root";
         String pw = "gina1768";
         Connection con = null;
         
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("드라이버 적재 성공 ... 데이터베이스에 연결중입니다.");
+            System.out.println("success ... Connecting database.");
             con = DriverManager.getConnection(url, id, pw);
-            System.out.println("데이터베이스 연결 성공");
+            System.out.println("Success to connect database");
         } catch (ClassNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -40,13 +44,13 @@ public class MainMenu {
 		String selectedMenuItem;
 		Scanner MenuItemScan = new Scanner(System.in);
 		
-		System.out.println("|-----------------------------|\n");
-		System.out.println("| 도서관 도서 관리 프로그램   |\n");
-		System.out.println("|-----------------------------|\n");
-		System.out.println("| 1. 로그인                   |\n");
-		System.out.println("| 2. 가입하기                 |\n");
-		System.out.println("|-----------------------------|\n");
-		System.out.println("메뉴를 선택하세요: ");
+		System.out.println("|--------------------------------|\n");
+		System.out.println("| Library book management system |\n");
+		System.out.println("|--------------------------------|\n");
+		System.out.println("|           1. Login             |\n");
+		System.out.println("|           2. Sing in           |\n");
+		System.out.println("|--------------------------------|\n");
+		System.out.println("press number : ");
 		
 		selectedMenuItem = MenuItemScan.nextLine();
 		
@@ -58,7 +62,7 @@ public class MainMenu {
 			registerStudent();
 			break;
 		default:
-			System.out.println("지원되지 않는 메뉴 번호입니다.");
+			System.out.println("The number is wrong.");
 			break;		
 		}
 		
@@ -70,17 +74,42 @@ public class MainMenu {
 		String password = null;
 		Scanner logindatascan = new Scanner(System.in);
 		
-		System.out.println("아이디와 비밀번호를 입력해주세요\n");
+		System.out.println("Press ID and Password\n");
 		System.out.println("ID: ");
 		id = logindatascan.nextLine();
 		System.out.println("Password: ");
 		password = logindatascan.nextLine();
-		
-		System.out.println("id: " + id +  "  password: " + password);
-		
-		goBackMenu();
-		
-		logindatascan.close();				
+		try {
+	          
+            Connection con = makeConnection();
+            Statement stmt = con.createStatement();           
+            String sql = "Select * from user where id="+id+";";
+            ResultSet rs =stmt.executeQuery(sql); 
+            while(rs.next()){
+                st_id = rs.getString("id");
+                st_pw = rs.getString("password");
+                st_name = rs.getString("name");
+                st_major = rs.getString("major");
+                st_type = rs.getString("type");                
+                //System.out.println(st_id+"\t"+st_pw+"\t"+st_name+"\t"+st_major+"\t"+st_type);
+                //System.out.println(st_id + id + st_pw + password );
+            } 
+            if((st_id.equals(id))&&(st_pw.equals(password))){
+            	goStudentMenu();
+            }
+            else
+            {
+            	System.out.print("ID and password are not match");
+                 goBackMenu();
+                 logindatascan.close(); //이부분 잘 모르겠어><
+            }
+            	
+            	
+           
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+						
 	}
 	
 	private void registerStudent(){
@@ -90,7 +119,7 @@ public class MainMenu {
 		String major;		
 		Scanner registerdatascan = new Scanner(System.in);
 		
-		System.out.println("새로운 학생 계정 생성하기\n아이디는 학번으로 입력해주세요.\n");
+		System.out.println("make new student account\nID must be same as your student number .\n");
 		System.out.println("ID: ");
 		id = registerdatascan.next();
 		System.out.println("Password: ");
@@ -102,37 +131,11 @@ public class MainMenu {
 		
 		System.out.println(id+ " " + password + "  " + name + " " + major + "  ");
 		try {
-            // 1. 연결 객체 만들기
-            Connection con = makeConnection();// 만약 이 문장이 try문 밖에 있으면 makeConnection()이 실행은 되지만
-            // Connection con에 저장하고. 아랫줄의 con.createStatement()에서 사용할 con과 연동이 되지 않을 수 있다!!(주의)
-            // 2. statment 객체 만들기
-            Statement stmt = con.createStatement();
-            // 3. ResultSet 만들기
-            String sql1 = "select * from student_db";
-            ResultSet rs = stmt.executeQuery(sql1);//프로시저 랭귀지 sql. 쿼리문만 바꾸는 랭귀지 기법
-            // 4. 데이터 추출하기.
-            while(rs.next()){
-                String st_id = rs.getString("studentid");
-                String st_pw = rs.getString("studentpw");
-                String st_name = rs.getString("studentname");
-                String st_major = rs.getString("studentmajor");
-                
-                
-                System.out.println(st_id+"\t"+st_pw+"\t"+st_name+"\t"+st_major);
-            }
-            
-            String sql2 = "Insert into student_db values('"+id+"','"+password+"','"+name+"','"+major+"')";
-            stmt.execute(sql2);
-            rs = stmt.executeQuery(sql1);
-            while(rs.next()){
-                String st_id = rs.getString("studentid");
-                String st_pw = rs.getString("studentpw");
-                String st_name = rs.getString("studentname");
-                String st_major = rs.getString("studentmajor");
-                
-                System.out.println(st_id+"\t"+st_pw+"\t"+st_name+"\t"+st_major);
-            }
-            
+          
+            Connection con = makeConnection();
+            Statement stmt = con.createStatement();           
+            String sql2 = "Insert into user values('"+id+"','"+password+"','"+name+"','"+major+"',0)";
+            stmt.execute(sql2);            
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -146,13 +149,18 @@ public class MainMenu {
 		int selectedMenu;
 		Scanner goMenuscan = new Scanner(System.in);
 		
-		System.out.println("\n\n메인 메뉴로 돌아가시겠습니까? 원하시면 1을 입력해주세요.");
+		System.out.println("\n\nDo you want to go abck to MainMenu? If you want press 1.");
 		selectedMenu = goMenuscan.nextInt();
 		if(selectedMenu == 1){
 			MainMenu.main(null);
 		}
 		else
-			System.out.println("1이 아닙니다."); //정하기
+			System.out.println("Number is not 1."); 
+	}
+	
+	private void goStudentMenu(){
+		Student st = new Student();
+    	st.StudentMenu();
 	}
 
 }
