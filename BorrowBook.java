4f634f2af764e33f26fe81a1e24683186f635cbb
joo6392 <joo6392@hanmail.net>
2014-12-06 22:26:54 +0900

    Update BorrowBook.java
    잘못된 책 번호 입력를 처리하는 것 수정

--- BorrowBook.java
+++ BorrowBook.java
@@ -5,23 +5,18 @@ import java.sql.SQLException;
 
 import com.mysql.jdbc.Connection;
 
-
 public class BorrowBook {
 	private MysqlUtils mysql = new MysqlUtils("localhost", "3306", "root", "1111", "library");
-	
-	/*public static void main(String[] args){
-		BorrowBook book = new BorrowBook();
-		//book.BorrowBook("1115669", "4");
-		//book.CheckBorrowedBookList("1110193");
-		book.CheckoutBorrowedBook("1110193", "2");
-	}	*/
 
 	private void BorrowBook(String userId, String selectBooknumber){	 //책 빌리기	
 		try{
 			mysql.connectDB();
 			String updateSQL = "UPDATE book SET borrow = "+ userId + " WHERE book_number = "+ selectBooknumber;
-			mysql.update(updateSQL);
-			System.out.println(selectBooknumber + "번 책이 반납되었습니다.");
+			if(mysql.update(updateSQL) > 0 )
+				System.out.println(selectBooknumber + "번 책이 반납되었습니다.");
+			else
+				System.out.println(selectBooknumber + "번 책은 없습니다.");
+
 		} catch(SQLException e1){
 			//e1.printStackTrace();
 			System.out.println("borrow failed");
@@ -29,41 +24,52 @@ public class BorrowBook {
 		}		
 	}
 	
-	void CheckBorrowedBookList(String userId){ //대출한 책 확인하기
+	int CheckBorrowedBookList(String userId){ //대출한 책 확인하기
+		int result = 1;
 		try{
-			mysql.connectDB();
-			
+			mysql.connectDB();			
 			String sql = "SELECT * FROM book WHERE borrow = " + userId;
-			ResultSet selectBook = mysql.query(sql);
-			System.out.println(userId + "님이 대출하신 책의 목록입니다.");
-			System.out.println("________________________");
-			System.out.println("book_number  " + " title");
-			System.out.println("------------------------");
-			
-			while(selectBook.next()){
-				int number = Integer.parseInt(selectBook.getString("book_number"));
-				System.out.println(" " + String.format("%05d", number) + "        " + selectBook.getString("title"));
+			ResultSet selectBook = mysql.query(sql);				
+			if(selectBook.next()){
+				System.out.println(userId + "님이 대출하신 책의 목록입니다.");
+				System.out.println("________________________");
+				System.out.println("book_number  " + " title");
+				System.out.println("------------------------");
+				selectBook.beforeFirst();
+				while(selectBook.next()){
+					int number = Integer.parseInt(selectBook.getString("book_number"));
+					System.out.println(" " + String.format("%05d", number) + "        " + selectBook.getString("title"));
+				}	
+				System.out.println("________________________");
 			}	
-			System.out.println("________________________");
+			else{
+				result = 2;
+				System.out.println(userId + "님이 대출하신 책은 없습니다.");
+			}				
+			
 		} catch(SQLException e1){
 			//e1.printStackTrace();
 			System.out.println("booklist failed");
 		} finally{			
-		}	
+		}
+		
+		return result;
 	}
  
 	void CheckoutBorrowedBook(String userId, String selectBooknumber){   //반납하기 
 		try{
 			mysql.connectDB();
 			String updateSQL = "UPDATE book SET borrow = NULL WHERE book_number = "+ selectBooknumber +" AND borrow = " + userId;
-			mysql.update(updateSQL);
-			System.out.println(selectBooknumber + "번 책이 반납되었습니다.");
+			if(mysql.update(updateSQL) > 0 ){
+				System.out.println(selectBooknumber + "번 책이 반납되었습니다.");
+			}
+			else{
+				System.out.println(selectBooknumber + "번 책은 없습니다.");
+			}
 		} catch(SQLException e1){
 			//e1.printStackTrace();
 			System.out.println("checkout failed");
 		} finally{			
 		}	
 	}
-
-
 }
