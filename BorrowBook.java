e4f1f4dae479fe21b449a1001b6870db070934ab
joo6392 <joo6392@hanmail.net>
2014-12-08 13:36:29 +0900

    Update BorrowBook.java
    대출 가능 여부 확인

--- BorrowBook.java
+++ BorrowBook.java
@@ -1,21 +1,51 @@
-
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.mysql.jdbc.Connection;
 
-public class BorrowBook {
-	private MysqlUtils mysql = new MysqlUtils("localhost", "3306", "root", "1111", "library");
 
-	private void BorrowBook(String userId, String selectBooknumber){	 //책 빌리기	
+public class BorrowBook {
+	private MysqlUtils mysql = new MysqlUtils("localhost", "3306", "root", "1111", "test");// "library");
+
+	int CheckBook(String userId, String selectBooknumber){
+		int check = 1; //1이면 대출 가능, 2이면 대출 불가능
 		try{
 			mysql.connectDB();
-			String updateSQL = "UPDATE book SET borrow = "+ userId + " WHERE book_number = "+ selectBooknumber;
-			if(mysql.update(updateSQL) > 0 )
-				System.out.println(selectBooknumber + "번 책이 반납되었습니다.");
-			else
-				System.out.println(selectBooknumber + "번 책은 없습니다.");
+			
+			String checkBorrowedSQL = "SELECT * FROM book WHERE book_number = "+ selectBooknumber;
+			ResultSet checkRs = mysql.query(checkBorrowedSQL);	
+			
+			if(checkRs.next()){
+				String result = checkRs.getString("borrow"); //null 대출 가능 / 같으면 대출 한거고 / 다르면 대출 못하고
+				if(result != null){
+					check = 2;
+					if(result.equals(userId))
+						System.out.println("이미 대출 하셨습니다. ");
+					else{
+						System.out.println("대출 불가능합니다. ");
+					}
+				}
+			}				
+
+		} catch(SQLException e1){
+			//e1.printStackTrace();
+			System.out.println("borrow failed");
+		} 	
+		
+		return check;
+	}
+	
+	void BorrowBook(String userId, String selectBooknumber){	 //책 빌리기	
+		try{
+			mysql.connectDB();
+			if(CheckBook(userId, selectBooknumber) == 1){
+				String updateSQL = "UPDATE book SET borrow = "+ userId + " WHERE book_number = "+ selectBooknumber;
+				if(mysql.update(updateSQL) > 0 )
+					System.out.println(selectBooknumber + "번 책이 대출되었습니다.");
+				else
+					System.out.println(selectBooknumber + "번 책은 없습니다.");
+			}
 
 		} catch(SQLException e1){
 			//e1.printStackTrace();
@@ -38,7 +68,7 @@ public class BorrowBook {
 				selectBook.beforeFirst();
 				while(selectBook.next()){
 					int number = Integer.parseInt(selectBook.getString("book_number"));
-					System.out.println(" " + String.format("%05d", number) + "        " + selectBook.getString("title"));
+					System.out.println(" " + String.format("%d", number) + "        " + selectBook.getString("title"));
 				}	
 				System.out.println("________________________");
 			}	
