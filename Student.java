b3a72b7ed3bc48143a5da86ab0a33ed10c0f3b94
joo6392 <joo6392@hanmail.net>
2014-12-08 13:38:55 +0900

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,4 +1,4 @@
-﻿import java.util.Scanner;
+import java.util.Scanner;
 
 public class Student {
 	String userId = "1110193";
@@ -11,13 +11,13 @@ public class Student {
 	public void StudentMenu(){
 		String selectedMenuItem;
 		Scanner MenuItemScan = new Scanner(System.in);
-		System.out.println("|-------------------------------------|\n");
-		System.out.println("|----------Student Main Menu----------|\n");
-		System.out.println("|           1. Search book            |\n");
-		System.out.println("|          2. current checkout        |\n"); 
-		System.out.println("|-------------------------------------|\n");
-		System.out.println(" Select Menu: ");
-	        selectedMenuItem = MenuItemScan.nextLine();
+		System.out.println("|-----------------------------|\n");
+		System.out.println("|----------학생 메인 메뉴----------|\n");
+		System.out.println("| 1. 책 검색하기 |\n");
+		System.out.println("| 2. 대출 현황 |\n");
+		System.out.println("|-----------------------------|\n");
+		System.out.println(" 메뉴를 선택하세요: ");
+		selectedMenuItem = MenuItemScan.nextLine();
 		
 		switch(selectedMenuItem){
 		case "1":
@@ -27,7 +27,7 @@ public class Student {
 			CheckBorrowedList();
 			break;
 		default:
-			System.out.println("The number you press is not apply");
+			System.out.println("지원되지 않는 메뉴 번호입니다.");
 			break;
 		}
 		MenuItemScan.close();
@@ -35,20 +35,29 @@ public class Student {
 
 	private void search(){
 		Scanner scnbook = new Scanner(System.in);
-		System.out.print("If you want to search, enter the part of book title. If you want to go back, press1");
+		System.out.print("책을 검색하시려면 책 제목의 일부를 입력해주세요. ");
 		String back = scnbook.nextLine();
-		switch(back){
-		case "1":
-			StudentMenu();
-			break;
-		default:
-			System.out.println("The number you pressed is not apply.");
-			break;
+		//책 제목이 들어왔을 경우 검색으로 가기
+		
+		StudentBook sbook = new StudentBook();
+		sbook.SearchBook(back);
+		//////////////////////////////////////////////////////////////////////////////  이거 고치기
+		System.out.print("자세히 보길 원하는 책 번호를 입력해주세요. ");
+		String selectedBookNumber = scnbook.nextLine();
+		sbook.showBookData(selectedBookNumber);
+		///////////////////////////////////////////////////////////////////////////////
+		System.out.print("대출을 원하시면 1을 입력해주세요. 이전 메뉴로 돌아가고 싶으시면 2을 입력해주세요. ");
+		String menu = scnbook.nextLine();
+		if(menu.equals("1")){
+			BorrowBook bbook = new BorrowBook();
+			bbook.BorrowBook(userId, selectedBookNumber);
 		}
+		StudentMenu();
 	}
 		
 	private void CheckBorrowedList(){
 		Scanner scncheck = new Scanner(System.in);
+		
 		BorrowBook borrowedbook = new BorrowBook();
 		int exitBorrowedbook = borrowedbook.CheckBorrowedBookList(userId);
 		
